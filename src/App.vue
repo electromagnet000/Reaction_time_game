6aa0f573051a3a4c2542613728c605e23c892597
electromagnet000 <sergiuhowie@campus.masterschool.com>
2024-05-05 17:50:11 +0100

    Created a simple Vue.js reaction time game

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <BlockReaction v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <GetResults v-if="showResults" :score="score"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BlockReaction from './components/BlockReaction.vue'
+import GetResults from './components/GetResults.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { BlockReaction, GetResults },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 2000 
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime,
+      this.isPlaying = false
+      this.showResults = true
+    }
   }
 }
 </script>
@@ -20,7 +41,20 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+
+button {
+  background: #1e1e1e;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+}
+
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/GetResults.vue
@@ -0,0 +1,35 @@
+<template>
+  <p>Reaction time : {{ score }} ms</p>
+  <p class="rank"> {{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+      return {
+        rank: null,
+      }
+    },
+    mounted() {
+      if (this.score < 100) {
+        this.rank = 'You are A GOD'
+      } else if (this.score < 200){
+        this.rank = "You're the reason your team won"
+      } else if (this.score < 400) {
+        this.rank = "You're the reason your team lost"
+      } else {
+        this.rank = 'that was shit Kill Yourself'
+      }
+    }
+}
+</script>
+
+<style>
+  .rank{
+    font-size: 30px;
+    color: blue;
+    font-weight: bold;
+
+  }
+</style>
